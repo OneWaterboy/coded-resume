b286ec42e34296ce0206e1cb3e27b40bdf79a3e5
Abrahm <abrahmrollins@gmail.com>
2024-03-26 17:38:51 -0700

    Changed experiences to JS Object to make changing resume easier

--- resume.js
+++ resume.js
@@ -1,25 +1,40 @@
 const skillsList = ['ReactJS', 'Vue.js', 'HTML5','CSS3', 'JavaScript', 'Node.js', 'Express.js', 'MongoDB', 'SQL', 'Python'];
 
-const awwExperience = [
-    'Collaborated closely with the back-end developers to integrate REST and GraphQL API calls into the front-end codebase.',
-    'Upgraded 45 legacy codebases from PHP 5 to PHP 8, enhancing, security and ensuring backward compatibility',
-    'Creation of over 50 UI components with reusable ReactJS code to reduce development time',
-    'Collaborated closely with the back-end developers to integrate REST and GraphQL API calls into the front-end codebase.',
-    'Use version control system Git to manage Github source code repositories for team collaboration.',
-    'Implement responsive design principles to ensure cross-platform compatibility of webpages.'
-]
 
-const fdgExperience = [ 
-    'Provided technical support when needed regarding any issues with existing WordPress websites.',
-    'Developed and maintained user-facing websites using HTML, CSS, JavaScript, and ReactJS.',
-    'Performed regular maintenance on 15 existing WordPress sites,including troubleshooting errors, updating plugins, and ensuring optimal functionality.',
-    'Developed 3 custom Shopify Apps by leveraging the REST admin API, enabling enhanced client functionality and customization',
-    'Participated in team code reviews, providing constructive feedback and suggestions to improve code quality, maintainability, and adherence to coding standards.'
-]
-
-const freelanceExperience = [
-    'Developed six custom WordPress themes for diverse clients, utilizing a combination of HTML, CSS, jQuery, and PHP.',
-    'Integrated third-party services such as Google Analytics into WordPress websites.',
-    'Created 2 custom plugins to streamline the website development workflow.',
-    
- ]
+ const experiences = {
+    "Alpine Web Works": {
+        position: "Front End Developer",
+        location: "Remote",
+        dates: "August 2022 - Present",
+        tasks: [
+            "Collaborated closely with the back-end developers to integrate REST and GraphQL API calls into the front-end codebase.",
+            "Upgraded 45 legacy codebases from PHP 5 to PHP 8, enhancing, security and ensuring backward compatibility",
+            "Creation of over 50 UI components with reusable ReactJS code to reduce development time",
+            "Collaborated closely with the back-end developers to integrate REST and GraphQL API calls into the front-end codebase.",
+            "Use version control system Git to manage Github source code repositories for team collaboration.",
+            "Implement responsive design principles to ensure cross-platform compatibility of webpages."
+        ]
+    },
+    "FDG Web": {
+        position: "Front End Developer",
+        location: "Hybrid - Remote",
+        dates: "March 2021 - August 2022",
+        tasks: [
+            "Provided technical support when needed regarding any issues with existing WordPress websites.",
+            "Developed and maintained user-facing websites using HTML, CSS, JavaScript, and ReactJS.",
+            "Performed regular maintenance on 15 existing WordPress sites,including troubleshooting errors, updating plugins, and ensuring optimal functionality.",
+            "Developed 3 custom Shopify Apps by leveraging the REST admin API, enabling enhanced client functionality and customization",
+            "Participated in team code reviews, providing constructive feedback and suggestions to improve code quality, maintainability, and adherence to coding standards."
+        ]
+    },
+    "Freelance": {
+        position: "WordPress Developer",
+        location: "Remote",
+        dates: "March 2019 - March 2021",
+        tasks: [
+            "Developed six custom WordPress themes for diverse clients, utilizing a combination of HTML, CSS, jQuery, and PHP.",
+            "Integrated third-party services such as Google Analytics into WordPress websites.",
+            "Created 2 custom plugins to streamline the website development workflow.",
+        ]
+    }
+};
